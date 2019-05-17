0211b87e855ea1c0b6c86ce47b6d41079c4d1e94
caofangsheng93 <fangsheng93@163.com>
2019-05-17 23:53:36 +0800

    0.0

--- WebAPIForCURD/Controllers/StudentController.cs
+++ WebAPIForCURD/Controllers/StudentController.cs
@@ -13,7 +13,7 @@ namespace WebAPIForCURD.Controllers
     /// 1.前面我们学过，方法以Get或者Get作为前缀，Web API就将其视为Get 请求的方法
     /// 2.我们不应该直接从Web API返回EF实体对象，推荐DTO（Data Transfer Object）数据传输对象。
     /// 3.请注意：ViewModel classes或者DTO classes仅仅是用于把数据从Web API控制器传递给客户端。你可以按照你自己的方式命名。
-    /// 4.下面的两个方法GetAllStudents()和GetAllStudentsWithGrade()，我们在编译的时候，不会报错，
+    /// 4.下面的两个注释的方法GetAllStudents()和GetAllStudentsWithGrade()，我们在编译的时候，不会报错，
     /// 但是运行的时候，就会报错，因为Web API不知道该执行哪个方法。
     /// 【找到了与该请求匹配的多个操作: 类型 WebAPIForCURD.Controllers.StudentController 
     /// 的 GetAllStudents 类型 WebAPIForCURD.Controllers.StudentController 的 GetAllStudentsWithGrade】；
@@ -22,11 +22,72 @@ namespace WebAPIForCURD.Controllers
     /// </summary>
     public class StudentController : ApiController
     {
+        #region 获取所有的Student记录
         /// <summary>
         /// 获取所有的Student记录
         /// </summary>
         /// <returns></returns>
-        public IHttpActionResult GetAllStudents()
+        //public IHttpActionResult GetAllStudents()
+        //{
+        //    //不应该直接返回EF对象，而应该返回DTO或者ViewModel对象
+        //    IList<StudentViewModel> students = null;
+        //    using (var db = new EFDemoDBEntities())
+        //    {
+        //        students = db.Students.Include("Grades").Select(s => new StudentViewModel()
+        //        {
+        //            ID = s.ID,
+        //            Name = s.Name,
+        //            Sex = s.Sex,
+        //            Age = s.Age
+
+        //        }).ToList<StudentViewModel>();
+        //    }
+        //    //count数量为0，说明没有数据
+        //    if (students.Count == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(students);
+        //}
+        #endregion
+
+        #region 获取所有的Student记录【包含Grade信息】
+        /// <summary>
+        /// 获取所有的Student记录【包含Grade信息】
+        /// </summary>
+        /// <returns></returns>
+        //public IHttpActionResult GetAllStudentsWithGrade()
+        //{
+        //    IList<StudentViewModel> students = null;
+        //    using (var db = new EFDemoDBEntities())
+        //    {
+        //        students = db.Students.Include("Grades").Select(s => new StudentViewModel()
+        //        {
+        //            ID = s.ID,
+        //            Name = s.Name,
+        //            Sex = s.Sex,
+        //            Age = s.Age,
+        //            Grade = s.Grade == null ? null : new GradeViewModel()
+        //            {
+        //                GradeID = s.Grade.GradeID,
+        //                GradeName = s.Grade.GradeName
+        //            }
+        //        }).ToList<StudentViewModel>();
+        //    }
+        //    if (students.Count == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(students);
+        //} 
+        #endregion
+
+        #region 获取所有的Student记录，包含Grade信息
+        /// <summary>
+        /// 获取所有的Student记录，包含Grade信息
+        /// </summary>
+        /// <returns></returns>
+        public IHttpActionResult GetAllStudents(bool includeGrade=false)
         {
             //不应该直接返回EF对象，而应该返回DTO或者ViewModel对象
             IList<StudentViewModel> students = null;
@@ -37,8 +98,12 @@ namespace WebAPIForCURD.Controllers
                     ID = s.ID,
                     Name = s.Name,
                     Sex = s.Sex,
-                    Age = s.Age
-
+                    Age = s.Age,
+                    Grade=s.Grade==null||includeGrade==false?null:new GradeViewModel()
+                    {
+                        GradeID=s.Grade.GradeID,
+                        GradeName=s.Grade.GradeName
+                    }
                 }).ToList<StudentViewModel>();
             }
             //count数量为0，说明没有数据
@@ -48,30 +113,6 @@ namespace WebAPIForCURD.Controllers
             }
             return Ok(students);
         }
-
-        public IHttpActionResult GetAllStudentsWithGrade()
-        {
-            IList<StudentViewModel> students = null;
-            using (var db = new EFDemoDBEntities())
-            {
-                students = db.Students.Include("Grades").Select(s => new StudentViewModel()
-                {
-                    ID = s.ID,
-                    Name = s.Name,
-                    Sex = s.Sex,
-                    Age = s.Age,
-                    Grade = s.Grade == null ? null : new GradeViewModel()
-                    {
-                        GradeID = s.Grade.GradeID,
-                        GradeName = s.Grade.GradeName
-                    }
-                }).ToList<StudentViewModel>();
-            }
-            if (students.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(students);
-        }
+        #endregion
     }
 }
