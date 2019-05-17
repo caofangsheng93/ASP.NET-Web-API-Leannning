f2529a343103e9226c07034ee390b7b3675d7e50
caofangsheng93 <fangsheng93@163.com>
2019-05-17 23:42:50 +0800

    Web API增删查改

---
+++ WebAPIForCURD/Models/GradeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace WebAPIForCURD.Models
+{
+    /// <summary>
+    /// Grade视图模型
+    /// </summary>
+    public class GradeViewModel
+    {
+        public int GradeID { get; set; }
+
+        public string GradeName { get; set; }
+
+        public ICollection<StudentViewModel> Students { get; set; }
+    }
+}
---
+++ WebAPIForCURD/Models/StudentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIForCURD.Models
+{
+    /// <summary>
+    /// Student视图模型
+    /// </summary>
+    public class StudentViewModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        public int Age { get; set; }
+
+        public GradeViewModel Grade { get; set; }
+    }
+}
